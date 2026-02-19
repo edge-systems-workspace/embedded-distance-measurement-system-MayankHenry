a7445a2f5c8b42eea86cbf2e73a1c87a8b8b90d6
MayankHenry <mayank.gla_cs.aiml24@gla.ac.in>
2026-02-19 09:18:26 +0530

    add editor configuration file and update main.cpp with pin definitions

--- src/main.cpp
+++ src/main.cpp
@@ -10,25 +10,19 @@
  * and displays structured output via Serial Monitor.
  */
 
- // TODO 1:
- // Define TRIG pin (Use pin 9)
+const int trigPin = 9;
+const int echoPin = 10;
 
- // TODO 2:
- // Define ECHO pin (Use pin 10)
 
- // TODO 3:
- // Create variable to store duration
+long duration = 0;
+float distance = 0;
 
- // TODO 4:
- // Create variable to store calculated distance
 
 void setup() {
 
-    // TODO 5:
-    // Initialize Serial communication (9600 baud rate)
+    Serial.begin(9600);
+    pinMode(trigPin, OUTPUT);
 
-    // TODO 6:
-    // Configure TRIG as OUTPUT
 
     // TODO 7:
     // Configure ECHO as INPUT
